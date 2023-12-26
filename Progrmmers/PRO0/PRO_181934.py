93173140a14c65382eb14923db6c38e15690c821
seobkeom <ajm153123@naver.com>
2023-12-26 20:10:16 +0900

    Feat : PRO_181934.py Code Update

--- Progrmmers/PRO0/PRO_181934.py
+++ Progrmmers/PRO0/PRO_181934.py
@@ -7,7 +7,6 @@ def solution(ineq, eq, n, m):
         if ineq==">":
             return int(n>=m)
         else: return int(n<=m)
-    return 0
 
 print(solution("<","=",20,50))
 print(solution(">","!",41,78))
